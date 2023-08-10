cfd4a5939bb09dd2bcd58b9cb52b48f05dd83ce8
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-08-10 16:14:11 +0330

    add home component

--- index.jsx
+++ index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Home from "./pages/Home";
 
 import "./server";
 
@@ -9,6 +10,12 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
   },
 ]);
 function App() {
--- layouts/Layout.jsx
+++ layouts/Layout.jsx
@@ -1,10 +1,14 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <div className="site-wrapper">
       <Header />
+      <main>
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
