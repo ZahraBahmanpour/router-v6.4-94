27c5a61a2bcddb0a6e22d4d04521238d4db58666
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-08-10 16:08:50 +0330

    add Layout

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <a href="/about">About</a>
+        <a href="/vans">Vans</a>
+      </nav>
+    </header>
+  );
+}
--- index.jsx
+++ index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./layouts/Layout";
 
 import "./server";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+  },
+]);
 function App() {
-  return <div>Hello</div>;
+  return <RouterProvider router={router} />;
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
