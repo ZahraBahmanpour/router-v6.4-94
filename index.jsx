a2ed50bc05f93d6f039c11b7b524725b9711d71f
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-08-10 15:55:13 +0330

    initial commit

---
+++ index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./server";
+
+function App() {
+  return <div>Hello</div>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
